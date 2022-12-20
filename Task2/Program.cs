22b3fc869df181d8785215ef3de144476cd8bbd4
Viki <viki333@list.ru>
2022-12-20 22:01:05 +0300

    Решили вторую задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число : ");
+int a = int.Parse(ReadLine()!);
+
+if ( a == 1 )
+    WriteLine("Понедельник");
+
+if ( a == 2 )
+    WriteLine("Вторник");
+
+if ( a == 3 )
+    WriteLine("Среда");
+
+if ( a == 4 )
+    WriteLine("Четверг");
+
+if ( a == 5 )
+    WriteLine("Пятница");
+
+if ( a == 6 )
+    WriteLine("Суббота");
+
+if ( a == 7 )
+    WriteLine("Воскресеньк");  
