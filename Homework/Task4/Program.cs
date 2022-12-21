d055564618825b384b630bcd3d2143903bd4ae0a
Viki <viki333@list.ru>
2022-12-21 17:06:00 +0300

    Add Task4

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите три числа:  ");
+int a = int.Parse(ReadLine()!);
+int b = int.Parse(ReadLine()!);
+int c = int.Parse(ReadLine()!);
+int max = a;
+
+if ( b > max ) {
+    max = b;
+}
+if (c >max ) {
+    max = c;
+}
+
+WriteLine($"Максимальнок число: {max} ");
+
+  
