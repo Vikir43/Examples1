2d015882d4e4bb4b3cc2400c842b1ac87b6b351d
Viki <viki333@list.ru>
2022-12-22 22:19:25 +0300

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int number = new Random().Next(100, 999);
+WriteLine(number);
+
+//int a1 = number/100;
+//int a2 = number%10;
+int a1 = number/100;
+int a2 = number%10;
+WriteLine($"{a1} {a2}");
+
+
+
