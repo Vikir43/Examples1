f49ea292c1d3209d2968a6b334630e5474b85c96
Viki <viki333@list.ru>
2022-12-23 19:50:34 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a :");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b : ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine( a*a==b || b*b == a?  "Да": "Нет");
+
