adc2bb642d7267a28b8eefca60a2aacd9f660e17
Viki <viki333@list.ru>
2022-12-23 10:07:06 +0300

    Add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+ Write("Введите первое число: ");
+ int a = int.Parse(ReadLine()!);
+ Write("Введите второе число: ");
+ int b = int.Parse(ReadLine()!);
+
+ //WriteLine("{a>b}? ">": $"{a<b}? "<":);
+ if (a>b)
+ {
+    WriteLine(">");
+ }
+ else
+ {
+    WriteLine ("<");
+ }
