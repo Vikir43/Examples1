e49c5b70decac37e4673ba9eb49b36eaae78cac5
Viki <viki333@list.ru>
2022-12-25 10:32:06 +0300

    Add Task13

--- Homework/Homework2/Task13/Program.cs
+++ Homework/Homework2/Task13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число : ");
+int a = int.Parse(ReadLine()!);
+
+if(a >99 )
+    if (a>999)
+       {
+        WriteLine($"  {a=a/100%10}");
+       }
+    else
+    {
+    WriteLine($"  {a%10}");
+    }
+else
+    {
+    WriteLine("Третьей цифры нет");
+    }
