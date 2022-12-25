eebe14bff1ae6c6bfa56b86fe9706c48b8aac3d1
Viki <viki333@list.ru>
2022-12-25 11:48:06 +0300

    Add Task15

--- Homework/Homework2/Task15/Program.cs
+++ Homework/Homework2/Task15/Program.cs
@@ -1,14 +1,15 @@
 ﻿using static System.Console;
 Clear();
 
-WriteLine("Введите число : ");
+WriteLine("Введите день недели от 1 до 7: ");
 int a = Convert.ToInt32(ReadLine()); 
 
-if (a< 6 )
-{
-    WriteLine("Не выходной день");
-}
-else
-{
-    WriteLine("Выходной день");
-}
+//if (a< 6 )
+//{
+  //  WriteLine("Не выходной день");
+//}
+//else
+//{
+  //  WriteLine("Выходной день");
+//}
+WriteLine(a < 6? "Не выходной день": a>7? "Нет такого дня недели": "Выходной день " );
