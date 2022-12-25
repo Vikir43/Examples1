5b55d3a29c3dfdbe3a84e614f663a70cb3cce239
Viki <viki333@list.ru>
2022-12-25 11:33:20 +0300

    Add Task15

---
+++ Homework/Homework2/Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число : ");
+int a = Convert.ToInt32(ReadLine()); 
+
+if (a< 6 )
+{
+    WriteLine("Не выходной день");
+}
+else
+{
+    WriteLine("Выходной день");
+}
