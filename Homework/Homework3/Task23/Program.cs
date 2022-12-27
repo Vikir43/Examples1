3e9aa6b4ce7d7ab3babd6ec0c1b6ab9e84a2bd7c
Viki <viki333@list.ru>
2022-12-27 17:01:17 +0300

    Add Task23

---
+++ Homework/Homework3/Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+int i=1;
+Write("Введите число для вывода кубов чисел от 1 до N:  ");
+int.TryParse(ReadLine(), out int n);
+
+
+while (i <n+1)  
+{
+    WriteLine($"{i} = {Math.Pow(i, 3)}");
+    i++;
+}
