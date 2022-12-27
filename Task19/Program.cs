6ed8eed54f1bf7ea2f3045ecace107ffdf0bb362
Viki <viki333@list.ru>
2022-12-27 12:35:06 +0300

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+
+Write("Точка А -> Введите координату x:  ");
+int ax  = int.Parse(ReadLine()!);
+Write("Точка А -> Введите координату y:  ");
+int ay  = int.Parse(ReadLine()!);
+Write("Точка B -> Введите координату x:  ");
+int bx  = int.Parse(ReadLine()!);
+Write("Точка B -> Введите координату y:  ");
+int by  = int.Parse(ReadLine()!);
+
+double d = Math.Sqrt(Math.Pow((bx-ax),2)+Math.Pow((by-ay),2));
+WriteLine(d);
+
+//int x = Math.Pow(ax-bx),2);
+//int y = Math.Pow((by-ay), 2);
+//int d = Math.Sqrt(x+y) 2;
+
