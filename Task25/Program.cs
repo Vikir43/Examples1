934427621a7fcb355400160f7aa9d05a3c6a4c2a
Viki <viki333@list.ru>
2023-01-19 14:59:19 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Принимает на вход число N и выдает количество цифр в числу
+// 78-2  789 - 3
+using static System.Console;
+Clear();
+Write("Введите число:  ");
+int.TryParse(ReadLine(), out int n);
+
+int number=NumCount(n);
+WriteLine($"Количество цифр в числе = {number}");
+
+
+int NumCount(int n)
+{
+    int result =0;
+    if (n/10==0)
+    return 1;
+    else
+    {
+       while(n>0)
+         {
+            n=n/10;
+            result+=1;
+         }
+    return result;
+    }
+}
