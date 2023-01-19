c71bc05cd66e67c20ed197b544e4a714cec379c9
Viki <viki333@list.ru>
2023-01-19 18:47:59 +0300

    Add Task25

--- Homework/Homework4/Task25/Program.cs
+++ Homework/Homework4/Task25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход два числа А и В и возводит число А в натуральную степень В с использованием цикла
+// 3,5 > 243   2,4 > 16
+using static System.Console;
+Clear();
+
+Write("Введите число A: ");
+int numberA = int.Parse(ReadLine()!);
+Write("Введите число B: ");
+int numberB = int.Parse(ReadLine()!);
+
+int exponent=Exponent(numberA, numberB);
+WriteLine($"Натуральная степень числа А возведенная в степень В = {exponent}");
+
+
+int Exponent(int numberA, int numberB)
+{
+    int result=1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
