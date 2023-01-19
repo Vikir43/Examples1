09d98ae80e1dc16f4bd962d25880ba9408aa89dd
Viki <viki333@list.ru>
2023-01-19 16:43:29 +0300

    Add Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке [0,1,0,10,1,0]
+using static System.Console;
+Clear();
+
+int [] arr = GetArray();
+
+GetArray ();
+PrintArray(arr);
+
+void FullArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+}
+
+int[] GetArray ()
+{
+    int [] array = new int [8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(2);
+    }
+    return array;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        WriteLine($"{array[i]}");
+    }
+}
