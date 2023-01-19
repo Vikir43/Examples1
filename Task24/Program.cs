5d8b1fe899ac5c3630a54206ba47aef7726914ff
Viki <viki333@list.ru>
2023-01-19 14:59:59 +0300

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,27 @@
+﻿// Принимает на вход число А и выдает сумму чисел от 1 до А
+using static System.Console;
+Clear();
+Write("Input number1:  ");
+int number1 = int.Parse(ReadLine()!);
+
+int sum1=GetSum(number1);
+WriteLine($"Sum number 1..A = {sum1}");
+
+Write("Input number2:  ");
+int number2 = int.Parse(ReadLine()!);
+
+int sum2=GetSum(number2);
+WriteLine($"Sum number 1..A = {sum2}");
+
+
+
+int GetSum(int A)
+{
+    int result=0;
+    while(A>0)
+    {
+        result += A;
+        A--;
+    }
+    return result;
+}
