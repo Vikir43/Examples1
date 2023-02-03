51045b8e0c0b6be13d8c55eb54b38ae638dd5092
Viki <viki333@list.ru>
2023-02-03 12:48:38 +0300

    Add Task32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу замена элементов массива: полож элементы на соответ отриц и наоборот
+// -4,-8,8,2  на 4,8,-8,-2
+using static System.Console;
+Clear();
+
+
+int[] array = {-4, -8, 8, 2};
+PrintArray(array);
+Console.WriteLine();
+
+ReverseArray(array);
+PrintArray(array);
+
+void ReverseArray(int[] inArray)
+{
+for (int i = 0; i <= inArray.Length-1; i++)
+    {
+        inArray[i] *= -1;
+    }
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
