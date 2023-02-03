16530d82ca179eb95303b9bac69f1d6689c7f93f
Viki <viki333@list.ru>
2023-02-03 15:16:46 +0300

    Add Task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив из 123 случайных чисел. Найти количество эл массива, значения которых лежат в отрезке от 10 до 99.
+using static System.Console;
+Clear();
+
+int[] array = GetArray(123, 0, 100);
+PrintArray(array);
+
+WriteLine();
+
+WriteLine($"Количество элементов массива в заданнои интервале равно: {ArrayCount(array)}");
+
+int ArrayCount(int[] InArray)
+{
+    int result = 0;
+    foreach(int i in InArray)
+    {
+        if(10 <= i && i <= 99) result++;
+    }
+    return result;
+}
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray=new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue +1);
+    }
+    return resultArray;
+}
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
