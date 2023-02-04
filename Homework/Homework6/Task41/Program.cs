22f5cbfd76d4bce54712806d7a65761c947dcdaf
Viki <viki333@list.ru>
2023-02-04 21:00:27 +0300

    Add Task41

---
+++ Homework/Homework6/Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+using static System.Console;
+
+Write("Введите числа через запятую: ");
+int [] arr = Array.ConvertAll(ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+   if(arr[i]>0)
+   {
+    count++;
+   } 
+}
+WriteLine($"Количество элементов в массиве > 0 = {count}");
